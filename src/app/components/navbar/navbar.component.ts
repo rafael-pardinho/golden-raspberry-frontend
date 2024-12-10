3887b830c0db256fdb483655ad89a13c48fb2caa
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-10 19:31:23 -0300

    feat: import class e interface

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
+  imports: [MatSidenavModule, MatListModule, MatToolbarModule, RouterModule],
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
