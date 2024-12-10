74885a24c5cbe302938cbf1be394ea3e8b7c4c98
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-10 19:23:23 -0300

    feat: configuração das rotas da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', loadComponent: () => import('./app/components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+      { path: 'list', loadComponent: () => import('./app/components/movie-list/movie-list.component').then(m => m.MovieListComponent) },
+    ]),
+    importProvidersFrom(BrowserAnimationsModule, HttpClientModule, MatSidenavModule),
+  ],
+});
