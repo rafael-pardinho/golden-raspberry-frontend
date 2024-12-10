6d267030427f3d1bc9f268a104f4f63f4514a560
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-10 20:06:57 -0300

    feat: ajuste no app.component import class e interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterModule, NavbarComponent],
 })
-export class AppComponent {
-  title = 'golden-raspberry-frontend';
-}
+export class AppComponent {}
