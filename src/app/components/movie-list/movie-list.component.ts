c55094319b089fa7b44aef0c7fc91d35b9eba39e
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-10 18:33:28 -0300

    feat: criado os components e services

---
+++ src/app/components/movie-list/movie-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  imports: [],
+  templateUrl: './movie-list.component.html',
+  styleUrl: './movie-list.component.scss'
+})
+export class MovieListComponent {
+
+}
