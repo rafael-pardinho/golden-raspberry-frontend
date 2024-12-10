008e400f3bebf2c3daa1ed278ff2411e53713618
Rafael Pardinho Lacerda <rafael.pardinho@rocketclouds.com.br>
2024-12-10 19:07:16 -0300

    feat: imports das class e interfaces

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  providers: [MovieService],
+  imports: [CommonModule, FormsModule],
 })
 export class DashboardComponent {
 
